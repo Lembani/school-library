82451a9045c9b8878b58de6c6e2121b30b0abf69
Lembani Sakala <lembanisakala@gmail.com>
2022-07-20 12:35:44 +0200

    FIx linter errors

--- student.rb
+++ student.rb
@@ -1,11 +1,11 @@
-# Copyright (C) Lembani Sakala - All rights reserved. 
-# Unauthorized copying of this file, via any medium is strictly prohibited. 
+# Copyright (C) Lembani Sakala - All rights reserved.
+# Unauthorized copying of this file, via any medium is strictly prohibited.
 # Proprietary and confidential.
 
 require_relative './person'
 
 class Student < Person
-  def initialize(classrom, name: 'Unknown', age, parent_permission: true)
+  def initialize(age, classrom, name: 'Unknown', parent_permission: true)
     super(name, age, parent_permission)
     @classrom = classrom
   end
