8ce04025d0102bbcc48bef69a10f71edc9eec436
Lembani Sakala <lembanisakala@gmail.com>
2022-07-20 12:35:50 +0200

    FIx linter errors

--- teacher.rb
+++ teacher.rb
@@ -1,11 +1,11 @@
-# Copyright (C) Lembani Sakala - All rights reserved. 
-# Unauthorized copying of this file, via any medium is strictly prohibited. 
+# Copyright (C) Lembani Sakala - All rights reserved.
+# Unauthorized copying of this file, via any medium is strictly prohibited.
 # Proprietary and confidential.
 
 require_relative './person'
 
 class Teacher < Person
-  def initialize(specialization, name: 'Unknown', age, parent_permission: true)
+  def initialize(specialization, age, name: 'Unknown', parent_permission: true)
     super(name, age, parent_permission)
     @specialization = specialization
   end
