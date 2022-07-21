b0a1e38c31bfd77c4f7935c317184299fae38aa0
Lembani Sakala <lembanisakala@gmail.com>
2022-07-21 18:02:32 +0200

    Update Person to inherit Nameable

--- person.rb
+++ person.rb
@@ -2,26 +2,32 @@
 # Unauthorized copying of this file, via any medium is strictly prohibited.
 # Proprietary and confidential.
 
+require_relative './nameable'
 require 'securerandom'
 
-class Person
+class Person < Nameable
+  attr_reader :id
+  attr_accessor :name, :age
+
   def initialize(age, name: 'Unknown', parent_permission: true)
+    super()
     @id = SecureRandom.uuid
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-  attr_reader :id
-  attr_accessor :name, :age
-
   def of_age?
     @age >= 18
   end
+
+  private :of_age?
 
   def can_use_service?
     @parent_permission == true || of_age?
   end
 
-  private :of_age?
+  def correct_name
+    @name
+  end
 end
