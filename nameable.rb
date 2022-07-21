1f762deb5c3ac1a31079dacb302df08947563ce3
Lembani Sakala <lembanisakala@gmail.com>
2022-07-21 18:01:40 +0200

    Create Nameable class: - add NotImplementedError method

---
+++ nameable.rb
@@ -0,0 +1,9 @@
+# Copyright (C) Lembani Sakala - All rights reserved.
+# Unauthorized copying of this file, via any medium is strictly prohibited.
+# Proprietary and confidential.
+
+class Nameable
+  def correct_name
+    raise NotImplementedError
+  end
+end
