7f5e085dec71fcd8e75488602b4ad08a2075e91a
Lembani Sakala <lembanisakala@gmail.com>
2022-07-22 12:40:21 +0200

    Update Person class

--- person.rb
+++ person.rb
@@ -10,7 +10,7 @@ class Person < Nameable
   attr_accessor :name, :age
 
   def initialize(age, name: 'Unknown', parent_permission: true)
-    super()
+    super
     @id = SecureRandom.uuid
     @name = name
     @age = age
