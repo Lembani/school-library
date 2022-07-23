a79d70a9c44e3b03aeeff7062a20941d9c5807de
Lembani Sakala <lembanisakala@gmail.com>
2022-07-23 10:23:08 +0200

    Add rentals[] & add_rental method to Book

--- book.rb
+++ book.rb
@@ -3,10 +3,15 @@
 # Proprietary and confidential.
 
 class Book
-  attr_accessor :title, :author
+  attr_accessor :title, :author, :rentals
 
   def initialize(title, author)
     @title = title
     @author = author
+    @rentals = []
+  end
+
+  def add_rental(date, person)
+    Rental.new(date, person, self)
   end
 end
