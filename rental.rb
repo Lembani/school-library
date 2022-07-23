04ef7c1667b28c06f56f2a78906c97d38d5226a6
Lembani Sakala <lembanisakala@gmail.com>
2022-07-23 17:47:08 +0200

    Update Rental

--- rental.rb
+++ rental.rb
@@ -5,12 +5,13 @@
 class Rental
   attr_accessor :date, :book, :person
 
-  def initialize(date, book, person)
+  def initialize(date, person, book)
     @date = date
-    @book = book
-    @person = person
 
+    @book = book
     book.rentals << self
+
+    @person = person
     person.rentals << self
   end
 end
