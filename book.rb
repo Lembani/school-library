c5562a8aace3c4ca7812fdfd6df27e2bb9651043
Lembani Sakala <lembanisakala@gmail.com>
2022-07-23 17:45:39 +0200

    Update Book class

--- book.rb
+++ book.rb
@@ -2,16 +2,19 @@
 # Unauthorized copying of this file, via any medium is strictly prohibited.
 # Proprietary and confidential.
 
+require_relative './rental'
+
 class Book
   attr_accessor :title, :author, :rentals
 
   def initialize(title, author)
+    @id = Random.rand(1...1000)
     @title = title
     @author = author
     @rentals = []
   end
 
   def add_rental(date, person)
-    Rental.new(date, person, self)
+    Rental.new(date, self, person)
   end
 end
